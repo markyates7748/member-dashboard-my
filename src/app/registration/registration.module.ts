a8bf69bd8fe0049abdfbe5f8a0069664a6ff3187
leandroyabut <leandro.yabut@smoothstack.com>
2021-08-01 16:11:58 -0600

    AF-236 Add registration page styling. Update font.

--- src/app/registration/registration.module.ts
+++ src/app/registration/registration.module.ts
@@ -1,12 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RegistrationComponent} from './registration.component';
+import { LookupFormComponent } from './lookup-form/lookup-form.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
 
 
 @NgModule({
   declarations: [
-    RegistrationComponent
+    RegistrationComponent,
+    LookupFormComponent,
+    RegistrationFormComponent
   ],
   imports: [
     CommonModule
