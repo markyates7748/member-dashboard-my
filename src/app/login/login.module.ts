966bf7ab7683297b24a083fbe7ae36102172ffbc
leandroyabut <leandro.yabut@smoothstack.com>
2021-07-31 23:47:02 -0600

    AF-236 Add login form and eslint

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    LoginFormComponent
   ],
   imports: [
     CommonModule
