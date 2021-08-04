5a37d3422dd284930f571379057bf40871f7ae3c
leandroyabut <leandro.yabut@smoothstack.com>
2021-08-03 23:48:15 -0600

    Remove global modal from app service.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
 import {filter, map} from 'rxjs/operators';
 import {Title} from '@angular/platform-browser';
 import {AppRoutingTitles} from './app-routing.titles';
-import {GlobalModalService} from '@app/global-modal/global-modal.service';
 
 @Component({
   selector: 'app-root',
@@ -14,8 +13,7 @@ export class AppComponent implements OnInit {
 
   constructor(private router: Router,
               private activatedRoute: ActivatedRoute,
-              private title: Title,
-              private modal: GlobalModalService) {
+              private title: Title) {
   }
 
   ngOnInit(): void {
@@ -32,6 +30,5 @@ export class AppComponent implements OnInit {
     ).subscribe((title: string) =>  {
       this.title.setTitle(`${AppRoutingTitles.APP} - ${title}`);
     });
-    this.modal.show({title: 'Test Title', message: 'Test Message'});
   }
 }
