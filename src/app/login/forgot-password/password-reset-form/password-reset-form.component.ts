3f4fed0a4a7471b4888fcc5fb8b68efca53b367c
leandroyabut <leandro.yabut@smoothstack.com>
2021-08-20 19:40:24 -0600

    Update style.

--- src/app/login/forgot-password/password-reset-form/password-reset-form.component.ts
+++ src/app/login/forgot-password/password-reset-form/password-reset-form.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, TemplateRef, ViewChild, ViewContainerRef} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-password-reset-form',
   templateUrl: './password-reset-form.component.html',
   styleUrls: ['./password-reset-form.component.sass']
 })
-export class PasswordResetFormComponent implements OnInit {
+export class PasswordResetFormComponent implements AfterViewInit {
 
-  constructor() { }
+  @ViewChild('sendOtp', {read: TemplateRef})
+  sendOtp!: TemplateRef<any>;
 
-  ngOnInit(): void {
+  @ViewChild('formContainer', {read: ViewContainerRef})
+  formContainer!: ViewContainerRef;
+
+  templates!: TemplateRef<any>[];
+
+  currentTemplate = 0;
+
+  createOtpForm: FormGroup;
+
+  constructor() {
+    this.createOtpForm = new FormGroup({
+      'username': new FormControl(''),
+      'email': new FormControl('')
+    });
   }
+
+  ngAfterViewInit(): void {
+    this.templates = [
+      this.sendOtp
+    ];
+    this.formContainer.createEmbeddedView(this.templates[this.currentTemplate]);
+  }
+
+
 
 }
