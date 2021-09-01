8f09c0692a7c2637faf934fa865521a42be7b085
leandroyabut <leandro.yabut@smoothstack.com>
2021-09-01 14:05:40 -0600

    AF-258 Add auth interceptor to core module.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,5 @@
 import {NgModule} from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {ErrorHandlerModule} from '@error-handler/error-handler.module';
 import {SingleCharDirective} from '@core/directives/single-char.directive';
 import {SingleCharInputComponent} from '@core/components/single-char-input/single-char-input.component';
@@ -9,6 +9,7 @@ import {FormControlIndicatorComponent} from '@core/components/form-control-indic
 import {FormControlValidationIndicatorComponent} from '@core/components/form-control-indicator/form-control-validation-indicator.component';
 import {AppIconsModule} from '@app/app-icons.module';
 import {NgbTooltipModule} from '@ng-bootstrap/ng-bootstrap';
+import {AuthInterceptor} from '@core/interceptors/auth.interceptor';
 
 
 @NgModule({
@@ -30,6 +31,13 @@ import {NgbTooltipModule} from '@ng-bootstrap/ng-bootstrap';
     HttpClientModule,
     SingleCharDirective,
     FormControlValidationIndicatorComponent
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
   ]
 })
 export class CoreModule { }
