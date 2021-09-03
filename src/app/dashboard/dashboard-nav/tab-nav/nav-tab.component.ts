b5e7f958d6c16d2bbb62dbd11f7d055bbacd8fe2
leandroyabut <leandro.yabut@smoothstack.com>
2021-09-02 23:08:02 -0600

    AF-266 Update padding on the navigation bottom tabs.

--- src/app/dashboard/dashboard-nav/tab-nav/nav-tab.component.ts
+++ src/app/dashboard/dashboard-nav/tab-nav/nav-tab.component.ts
@@ -4,7 +4,7 @@ import {Tab} from '@dashboard/dashboard-nav/tab-nav/tab-nav.component';
 @Component({
   selector: 'app-nav-tab',
   template: `
-    <button [routerLink]="tab.route" class="btn text-light p-4 d-flex flex-column justify-content-center align-items-center w-100">
+    <button [routerLink]="tab.route" class="btn text-light p-3 d-flex flex-column justify-content-center align-items-center w-100">
       <fa-icon [icon]="tab.icon" class="fs-3"></fa-icon>
       <span class="fw-light d-inline-block mt-1" style="font-size: small">{{tab.label}}</span>
     </button>
