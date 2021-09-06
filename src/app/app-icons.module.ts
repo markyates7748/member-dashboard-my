c12e8fa2d827161f5a4ee8ec40641901b2f54cb8
leandroyabut <leandro.yabut@smoothstack.com>
2021-09-06 00:07:55 -0600

    AF-266 Make default user icon foreground dark.

--- src/app/app-icons.module.ts
+++ src/app/app-icons.module.ts
@@ -2,14 +2,14 @@ import {NgModule} from '@angular/core';
 import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {IconDefinition} from '@fortawesome/fontawesome-svg-core';
 import {
-  faAngleRight, faArrowRight, faBars, faChartBar, faCheckCircle,
+  faAngleRight, faArrowRight, faBars, faChartBar, faCheckCircle, faCircle,
   faExclamationCircle, faIdCard,
   faInfoCircle,
   faShieldAlt, faTags, faTimesCircle, faUnlockAlt,
   faUserCheck, faUserCircle,
   faUserPlus, faWallet
 } from '@fortawesome/free-solid-svg-icons';
-import {faCircle} from '@fortawesome/free-regular-svg-icons';
+import {faCircle as regCircle} from '@fortawesome/free-regular-svg-icons';
 
 const icons: IconDefinition[] = [
   faAngleRight,
@@ -21,14 +21,15 @@ const icons: IconDefinition[] = [
   faCheckCircle,
   faTimesCircle,
   faUnlockAlt,
-  faCircle,
+  regCircle,
   faBars,
   faChartBar,
   faWallet,
   faTags,
   faUserCircle,
   faArrowRight,
-  faIdCard
+  faIdCard,
+  faCircle
 ];
 
 @NgModule({
