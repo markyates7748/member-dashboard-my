d74a1ce04c1cca0b302f83c01a94cc417e3fec17
leandroyabut <leandro.yabut@smoothstack.com>
2021-09-21 16:29:57 -0600

    Switch to use https api.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   application: {
-    api: 'http://api.alinefinancial.com/api',
+    api: 'https://api.alinefinancial.com/api',
     landingPortal: 'https://alinefinancial.com'
   }
 };
