871c886fad2a7d525d46d86bfea4fc9069c3cf64
leandroyabut <leandro.yabut@smoothstack.com>
2021-09-30 14:28:35 -0600

    AF-283 Fix tests and add new icons

--- src/app/core/services/transaction.service.spec.ts
+++ src/app/core/services/transaction.service.spec.ts
@@ -1,12 +1,20 @@
 import {TestBed} from '@angular/core/testing';
 
 import {TransactionService} from './transaction.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('TransactionService', () => {
   let service: TransactionService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        TransactionService
+      ]
+    });
     service = TestBed.inject(TransactionService);
   });
 
